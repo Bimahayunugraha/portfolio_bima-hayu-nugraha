6fc5fc0891d7669af3ae01e50442f758230ffaba
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2023-01-11 17:52:29 +0700

    feat/home: added project data and redux

---
+++ src/data.jsx
@@ -0,0 +1,63 @@
+import project1 from "./assets/img/png/project-1.png";
+import project2 from "./assets/img/png/project-2.png";
+import project3 from "./assets/img/png/project-3.png";
+import project4 from "./assets/img/png/project-4.png";
+import project5 from "./assets/img/png/project-5.png";
+import project6 from "./assets/img/png/project-6.png";
+
+const projectsData = [
+	{
+		id: 1,
+		title: "YuNotes",
+		category: "Android Application",
+		image: project1,
+		link: "https://github.com/Bimahayunugraha/YuNotes",
+		description:
+			"YuNotes adalah aplikasi android yang digunakan untuk mencatat seperti catatan tugas ataupun lainnya.",
+	},
+	{
+		id: 2,
+		title: "Data Karyawan PT Sawit Bahagia",
+		category: "Web Application",
+		image: project2,
+		link: "https://github.com/Bimahayunugraha/DataKaryawan_PTSawitBahagia",
+		description: "Website yang digunakan untuk mengelola data karyawan",
+	},
+	{
+		id: 3,
+		title: "Survey Pelanggan",
+		category: "Web Application",
+		image: project3,
+		link: "https://github.com/Bimahayunugraha/KP_Project_SurveyPelanggan_ConsumeFlaskAPIWithLaravel",
+		description:
+			"Website yang digunakan untuk menampilkan hasil olahan data survey pelanggan LifeMedia.",
+	},
+	{
+		id: 4,
+		title: "PonPin",
+		category: "Web Application",
+		image: project4,
+		link: "https://github.com/Bimahayunugraha/sc_website-ponpin",
+		description:
+			"Website yang digunakan untuk melakukan penilaian terhadap kinerja dari pak RT dalam menyediakan fasilitas.",
+	},
+	{
+		id: 5,
+		title: "eRekap",
+		category: "Web Application",
+		image: project5,
+		link: "https://github.com/Bimahayunugraha/erekap",
+		description: "Website yang digunakan untuk melakukan rekap nilai siswa.",
+	},
+	{
+		id: 6,
+		title: "Workfit",
+		category: "Web Application",
+		image: project6,
+		link: "https://github.com/Capstone-Project-Kelompok-6/Fe-React",
+		description:
+			"Website yang digunakan untuk mengelola data membership, workout, instructor, offline dan online class, offline dan online booking, newsletter, dan content pada sebuah gym.",
+	},
+];
+
+export default projectsData;
