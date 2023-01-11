e3470f92b696a54acbe64842f74408d8764622d8
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2023-01-11 17:53:11 +0700

    feat/home: added mock navbar collection

---
+++ src/mocks/navbarCollection.jsx
@@ -0,0 +1,14 @@
+export const navbarCollections = [
+	{
+		name: "Home",
+		path: "#home",
+	},
+	{
+		name: "Tentang Saya",
+		path: "#tentang",
+	},
+	{
+		name: "Project",
+		path: "#project",
+	},
+];
